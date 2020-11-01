8e476dfededde38c9aedd41247bc3fa20e0e2040
RedSmarty <64510423+RedSmarty@users.noreply.github.com>
2020-11-01 14:55:18 +0530

    Update Info.java

--- src/main/java/net/redsmarty/FreezerMC/Info.java
+++ src/main/java/net/redsmarty/FreezerMC/Info.java
@@ -1,5 +1,5 @@
 public class Info {
-  private Version serverVersion;
+  private Version serverVersion = new Version("server", 1.0, "idk", "lol");)
   
   public static Version getServerVersion() {
       return serverVersion;
