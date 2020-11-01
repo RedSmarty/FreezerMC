211298b8929939d87e3e71bf66f6b9c20b63f851
RedSmarty <64510423+RedSmarty@users.noreply.github.com>
2020-11-01 13:33:54 +0530

    Create Version.java

---
+++ src/main/java/net/redsmarty/FreezerMC/DataTypes/Version.java
@@ -0,0 +1,12 @@
+public version Version {
+  private String versionType;
+  private int versionNumber;
+  private String gitRepo;
+  private boolean nottifyOnUpdate
+  public Version(String versionType, int versionNumber, String gitRepo, boolean notifyOnUpdate) {
+    this.versionType = versionType;
+    this.versionNumber = versionNumber;
+    this.gitRepo = gitRepo;
+    this.notifyOnUpdate = notifyOnUpdate;
+  }
+}
