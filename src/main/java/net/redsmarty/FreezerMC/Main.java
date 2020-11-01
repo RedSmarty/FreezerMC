02ce9801f6ad770ce042ed7b06180f5155376b45
RedSmarty <64510423+RedSmarty@users.noreply.github.com>
2020-11-01 13:22:41 +0530

    Create Main.java

---
+++ src/main/java/net/redsmarty/FreezerMC/Main.java
@@ -0,0 +1,7 @@
+import net.redsmarty.FreezerMC.Info.java;
+
+public class Main {
+  public static void main(String[] args) {
+     System.out.println("Starting FreezerMC".concat(Info.getVersion));
+  }
+}
